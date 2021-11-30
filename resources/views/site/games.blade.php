3cf0c79aff10903f5a33c44ed39a0d6a0b476086
Ruan Silva <71744272+Superhardcgs@users.noreply.github.com>
2021-11-30 14:16:35 -0300

    polimento games
    polimento da tela de games

--- resources/views/site/games.blade.php
+++ resources/views/site/games.blade.php
@@ -84,12 +84,12 @@
     
     <main>
   <div class="px-4 py-5 my-5 text-center">
-  <h1 class="display-5 fw-bold">Plataformas Streaming</h1>
+  <h1 class="display-5 fw-bold">Notícia de Jogos</h1>
     </br>
-    <img class="d-block mx-auto mb-4" src="https://files.tecnoblog.net/wp-content/uploads/2018/12/steam-logo-700x394.jpg" alt="" width="640" height="360">
-    <h1 class="display-5 fw-bold">N</h1>
+    <img class="d-block mx-auto mb-4" src="https://adrenaline.com.br/uploads/chamadas/20JogosNaoDivulgadosEAGamePass.jpg" alt="" width="640" height="360">
+    <h1 class="display-5 fw-bold">Game News</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <p class="lead mb-4">Notícias importantes sobre jogos da atualidade no mundo Gamer</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
         <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
